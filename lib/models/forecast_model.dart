b22011265d0defbd31b21ab9fc3a2b09702ea336
Alexander Kobets <renbrane@gmail.com>
2021-04-01 17:05:34 +0300

    Исправлено всё, кроме использования HTTP и написанной вручную логики парсинга. В home_screen.dart была проблема с неймингом (buildErrorUI и прочие), тоже исправлено

--- lib/models/forecast_model.dart
+++ lib/models/forecast_model.dart
@@ -7,25 +7,25 @@ class ForecastData {
   ForecastData({this.listHourly, this.listDaily});
 
   factory ForecastData.fromJson(Map<String, dynamic> json) {
-    List listHourly = new List();
-    List listDaily = new List();
+    List listHourly;
+    List listDaily;
 
     for (dynamic e in json['hourly']) {
-      HourlyWeatherData w = new HourlyWeatherData(
-          date: new DateTime.fromMillisecondsSinceEpoch(e['dt'] * 1000,
-              isUtc: false),
-          temp: e['temp'].toDouble(),
-          main: e['weather'][0]['main'],
-          icon: e['weather'][0]['icon']);
+      HourlyWeatherData w = HourlyWeatherData(
+        date: DateTime.fromMillisecondsSinceEpoch(e['dt'] * 1000, isUtc: false),
+        temp: e['temp'].toDouble(),
+        main: e['weather'][0]['main'],
+        icon: e['weather'][0]['icon'],
+      );
       listHourly.add(w);
     }
     for (dynamic c in json['daily']) {
-      DailyWeatherData w = new DailyWeatherData(
-          date: new DateTime.fromMillisecondsSinceEpoch(c['dt'] * 1000,
-              isUtc: false),
-          temp: c['temp']['day'].toDouble(),
-          main: c['weather'][0]['main'],
-          icon: c['weather'][0]['icon']);
+      DailyWeatherData w = DailyWeatherData(
+        date: DateTime.fromMillisecondsSinceEpoch(c['dt'] * 1000, isUtc: false),
+        temp: c['temp']['day'].toDouble(),
+        main: c['weather'][0]['main'],
+        icon: c['weather'][0]['icon'],
+      );
       listDaily.add(w);
     }
 
